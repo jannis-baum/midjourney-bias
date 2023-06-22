60f6d8f33a3b6b4181ea1dbe87f837a7be7b70fa
Jannis Baum <jannis.baum@gmail.com>
2023-06-22 19:47:16 +0200

    refactor: separate canvases

--- metric-visualization/visualization.js
+++ metric-visualization/visualization.js
@@ -1,25 +1,47 @@
-new p5((sketch) => {
-    const w = window.innerWidth;
-    const h = window.innerHeight;
-    const spacer = 15;
+// -----------------------------------------------------------------------------
+// metrics & population based on sliders ---------------------------------------
 
-    const radius = (w*2/3 - spacer * 3)/4
-    const orbits = 10;
-    const entityDist = radius/orbits;
-    const entitySize = entityDist * 0.75;
-    const xPos = spacer + radius;
-    const yPos = xPos;
-    const xNeg = 2*spacer + 3*radius;
-    const yNeg = yPos;
-    const xBars = w*2/3 + spacer;
-    const wBars = w/3 - 2 * spacer;
-    
+let labelPos, labelNeg;
+let truePos, falsePos;
+let FDR, FOR, FPR, FNR;
+let n = 0;
+
+function updateValues() {
+    const truth = document.getElementById('sliderTruth').value / 1000;
+    const predP = document.getElementById('sliderPredP').value / 1000;
+    const predN = document.getElementById('sliderPredN').value / 1000;
+
+    labelPos = n * truth;
+    labelNeg = n - labelPos;
+    truePos = labelPos * predP;
+    falsePos = labelNeg * (1 - predN);
+
+    const falseNeg = labelPos - truePos;
+    const trueNeg = labelNeg - falsePos;
+    const predPos = falsePos + truePos;
+    const predNeg = falseNeg + trueNeg;
+
+    FDR = falsePos / predPos;
+    FOR = falseNeg / predNeg;
+    FPR = falsePos / labelNeg;
+    FNR = falseNeg / labelPos;
+}
+
+const colorPos = '#a8d2e7';
+const colorNeg = '#ff8880';
+
+// -----------------------------------------------------------------------------
+// population sketch -----------------------------------------------------------
+
+new p5((sketch) => {
+    let w, h;
+
+    let radius, orbits, entityDist, entitySize;
+    let xPos, yPos, xNeg, yNeg;
+
+    const spacer = 15;
     const noiseScalar = 15;
     const noiseSpeed = 0.05
-
-    const colorPos = '#a8d2e7';
-    const colorNeg = '#ff8880';
-    const colorBar = '#fcde71'
 
     const entityOffsets = (callback) => {
         let i = 0;
@@ -31,8 +53,6 @@ new p5((sketch) => {
             }
         }
     }
-    let n = 0;
-    entityOffsets(() => n += 1);
 
     const drawEntities = (x, y, maxCount, posCount) => {
         sketch.noStroke();
@@ -47,80 +67,71 @@ new p5((sketch) => {
         });
     }
 
-    const drawBar = (x, y, w, h, fill, label) => {
-        sketch.noStroke();
-        sketch.fill(colorBar);
-        sketch.rect(x, y, w * fill, h);
-        sketch.stroke('black');
-        sketch.noFill();
-        sketch.rect(x, y, w, h);
-        sketch.textAlign(sketch.LEFT, sketch.BOTTOM);
-        sketch.text(label, x, y - spacer, w);
-    }
-
-    let sliderTruth;
-    let sliderPredP;
-    let sliderPredN;
-
     sketch.setup = () => {
+        div = document.getElementById('canvas-population');
+
+        w = div.offsetWidth;
+        h = div.offsetHeight;
+        radius = (w - spacer) / 4
+        orbits = 10;
+        entityDist = radius/orbits;
+        entitySize = entityDist * 0.75;
+        xPos = radius;
+        yPos = h/2;
+        xNeg = spacer + 3*radius;
+        yNeg = yPos;
+        entityOffsets(() => n += 1);
+
+        updateValues();
+
         sketch.createCanvas(w, h);
-
-        const sliderY = yPos + spacer * 2 + radius
-        sliderTruth = sketch.createSlider(1, 1000, 500);
-        sliderTruth.position(spacer, sliderY);
-        sliderPredN = sketch.createSlider(1, 1000, 1000);
-        sliderPredN.position(spacer, sliderY + spacer * 2);
-        sliderPredP = sketch.createSlider(1, 1000, 1000);
-        sliderPredP.position(spacer, sliderY + spacer * 4);
-
         sketch.frameRate(30);
-    }
-
-    const labelSlider = (slider, text) => {
-        sketch.fill('black');
         sketch.noStroke();
-        sketch.textAlign(sketch.LEFT);
-        sketch.text(text, slider.width + spacer * 2, slider.y + spacer);
     }
 
     let noiseOffset = 0;
     sketch.draw = () => {
         sketch.background('white');
-        sketch.fill('#ccc');
-        sketch.rect(w*2/3, 0, w/3, h);
 
-        labelSlider(sliderTruth, 'Labeled positive rate')
-        labelSlider(sliderPredP, 'Rate of correct predictions for positive labels')
-        labelSlider(sliderPredN, 'Rate of correct predictions for negative labels')
-
-        const labelPos = n * (sliderTruth.value() / 1000);
-        const truePos = labelPos * (sliderPredP.value() / 1000);
-        const falseNeg = labelPos - truePos;
-
-        const labelNeg = n * (1 - sliderTruth.value() / 1000);
-        const falsePos = labelNeg * (1 -sliderPredN.value() / 1000);
-        const trueNeg = labelNeg - falsePos;
-
-        const predPos = falsePos + truePos;
-        const predNeg = falseNeg + trueNeg;
-
-        const FDR = falsePos / predPos;
-        const FOR = falseNeg / predNeg;
-        const FPR = falsePos / labelNeg;
-        const FNR = falseNeg / labelPos;
-
-        drawBar(xBars, spacer, wBars, 200, FDR, 'False Discovery Rate');
-        drawBar(xBars, 200 + 2*spacer, wBars, 200, FOR, 'False Omission Rate');
-        drawBar(xBars, 2*200 + 3*spacer, wBars, 200, FPR, 'False Positive Rate');
-        drawBar(xBars, 3*200 + 4*spacer, wBars, 200, FNR, 'False Negative Rate');
-
-        sketch.noStroke();
         sketch.fill(colorPos + '33');
         sketch.circle(xPos, yPos, radius * 2);
         drawEntities(xPos, yPos, labelPos, truePos);
+
         sketch.fill(colorNeg + '33');
         sketch.circle(xNeg, yNeg, radius * 2);
         drawEntities(xNeg, yNeg, labelNeg, falsePos);
+
         noiseOffset += noiseSpeed;
     }
-}, 'canvas');
+}, 'canvas-population');
+
+// -----------------------------------------------------------------------------
+// bars for metrics ------------------------------------------------------------
+
+const barSketch = (sketch, color, value, parent) => {
+    let w, h;
+
+    sketch.setup = () => {
+        p = document.getElementById(parent);
+        w = p.offsetWidth;
+        h = p.offsetHeight;
+        console.log(w, h)
+
+        sketch.createCanvas(w, h);
+        sketch.frameRate(30);
+        sketch.stroke('white');
+    }
+
+    sketch.draw = () => {
+        sketch.background('#444');
+        sketch.noFill();
+        sketch.rect(0, 0, w, h)
+        sketch.fill(color);
+        sketch.rect(0, 0, w * value(), h)
+    }
+}
+
+new p5((sketch) => barSketch(sketch, colorPos, () => FDR, 'canvas-bar-fdr'), 'canvas-bar-fdr');
+new p5((sketch) => barSketch(sketch, colorNeg, () => FOR, 'canvas-bar-for'), 'canvas-bar-for');
+new p5((sketch) => barSketch(sketch, colorPos, () => FPR, 'canvas-bar-fpr'), 'canvas-bar-fpr');
+new p5((sketch) => barSketch(sketch, colorNeg, () => FNR, 'canvas-bar-fnr'), 'canvas-bar-fnr');
