b2968ae51daf952bd2df7862aac42fe4fa879cd1
Jannis Baum <jannis.baum@gmail.com>
2023-06-22 08:31:23 +0200

    feat: wip bar chart

--- metric-visualization/visualization.js
+++ metric-visualization/visualization.js
@@ -12,6 +12,9 @@ new p5((sketch) => {
 
     const colorPos = '#a8d2e7';
     const colorNeg = '#ff8880';
+    const colorBar = '#fcde71'
+    
+    const spacer = 15;
 
     const entityLocations = (callback) => {
         let i = 0;
@@ -27,6 +30,7 @@ new p5((sketch) => {
     entityLocations(() => n += 1);
 
     const drawEntities = (x, y, maxCount, posCount) => {
+        sketch.noStroke();
         entityLocations((r, theta, i) => {
             if (i >= maxCount) return;
             sketch.fill(i >= posCount ? colorNeg : colorPos);
@@ -38,6 +42,17 @@ new p5((sketch) => {
         });
     }
 
+    const drawBar = (x, y, w, h, fill, label) => {
+        sketch.noStroke();
+        sketch.fill(colorBar);
+        sketch.rect(x, y + h * (1 - fill), w, h * fill);
+        sketch.stroke('black');
+        sketch.noFill();
+        sketch.rect(x, y, w, h);
+        sketch.textAlign(sketch.CENTER);
+        sketch.text(label, x, y + h + spacer, w);
+    }
+
     let sliderTruth;
     let sliderPredP;
     let sliderPredN;
@@ -45,22 +60,21 @@ new p5((sketch) => {
     sketch.setup = () => {
         sketch.createCanvas(w, h);
 
-        sliderTruth = sketch.createSlider(0, 1000, 500);
-        sliderTruth.position(20, 20);
-        sliderPredN = sketch.createSlider(0, 1000, 500);
-        sliderPredN.position(20, 50);
-        sliderPredP = sketch.createSlider(0, 1000, 500);
-        sliderPredP.position(20, 80);
+        sliderTruth = sketch.createSlider(1, 1000, 500);
+        sliderTruth.position(spacer, spacer);
+        sliderPredN = sketch.createSlider(1, 1000, 1000);
+        sliderPredN.position(spacer, spacer * 3);
+        sliderPredP = sketch.createSlider(1, 1000, 1000);
+        sliderPredP.position(spacer, spacer * 5);
 
         sketch.frameRate(30);
-        sketch.noFill();
-        sketch.stroke('black');
     }
 
     const labelSlider = (slider, text) => {
         sketch.fill('black');
         sketch.noStroke();
-        sketch.text(text, slider.x * 2 + slider.width, slider.y + 15);
+        sketch.textAlign(sketch.LEFT);
+        sketch.text(text, slider.width + spacer * 2, slider.y + spacer);
     }
 
     let noiseOffset = 0;
@@ -87,6 +101,12 @@ new p5((sketch) => {
         const FPR = falsePos / labelNeg;
         const FNR = falseNeg / labelPos;
 
+        drawBar(spacer, 200, 100, 200, FDR, 'False Discovery Rate');
+        drawBar(100 + 2 * spacer, 200, 100, 200, FOR, 'False Omission Rate');
+        drawBar(200 + 3 * spacer, 200, 100, 200, FPR, 'False Positive Rate');
+        drawBar(300 + 4 * spacer, 200, 100, 200, FNR, 'False Negative Rate');
+
+        sketch.noStroke();
         sketch.fill(colorPos + '33');
         sketch.circle(w/2 - w/5, h/2, (radius + entityDist) * 2);
         drawEntities(w/2 - w/5, h/2, labelPos, truePos);
